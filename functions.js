0dfdcd30965b3398c5dcb18d1643524907fda1eb
claudio86 <baldo.claudio.86@gmail.com>
2016-09-06 22:33:27 +0200

    test some functions

--- functions.js
+++ functions.js
@@ -1,9 +1,69 @@
+//LISTA GIOCATORI
 
-//ALGORITMO RANDOM
-function shuffle(array) {
+/*
+function creaGicoatori () {
+	var player_list = [];
+	for (i=0;object i<cards_per_player;i++){
+		player_list.push (i);
+	}
+}
+*/
+//PESCA
+function pesca(min, list) {
+  seme =  Math.floor(Math.random() * (Object.keys(list).length - min + 1) + min);
+}
+
+///DAI CARTA
+function daiCarta (id_pl) {
+	if (_.size(gameCards) != 0){
+		mischia(gameCards);
+	}
+	else {
+		return false;
+	}
+}
+
+///ASSEGNA ORDINE GIOCATORI
+function playerOrder (player_list) {
+	return mischia(player_list);
+}
+
+///SELEZIONA SEME
+function selSeme () {
+	return pesca(0,card_list); 
+}
+
+//NUOVO GIOCO
+function nuovoGioco (){
+	if (player_list.length == 3) {
+		gameCards = card_list;
+		trePlayers();console.log('3player');
+	}
+	else {
+		gameCards = card_list;console.log(gameCards);
+	}
+	selSeme ();
+	playerOrder (player_list);
+}
+
+///ELIMINA CARTA SE 3 GIOCATORI
+function trePlayers () {
+	var listaDue = ['d2','c2','b2','s2'];
+	var carta = [Math.floor(Math.random() * 4) + 1];
+	carta_rimossa = gameCards[listaDue[carta-1]];
+	delete gameCards[listaDue[carta-1]];
+}
+
+//LISTA CARTE IN MANO 
+function carteMano (id_pl) {
+	
+}
+
+///ALGORITMO RANDOM
+function mischia(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
 
-  // While there remain elements to shuffle...
+  // While there remain elements to mischia...
   while (0 !== currentIndex) {
 
     // Pick a remaining element...
@@ -15,42 +75,9 @@ function shuffle(array) {
     array[currentIndex] = array[randomIndex];
     array[randomIndex] = temporaryValue;
   }
-
   return array;
 }
 
-///DAI CARTA
-function daiCarta (id_pl) {
-	if (_.size(card_list) != 0){
-		shuffle(card_list);
-	}
-	else {
-		return false;
-	}
-}
-
-///ASSEGNA ORDINE GIOCATORI
-var player_list = ["Pippo","Pluto"];
-function playerOrder (player_list) {
-	return shuffle(player_list);
-}
-
-///SELEZIONA SEME
-function selSeme () {
-	return shuffle(card_list); 
-}
-
-//ELIMINA CARTA SE 3 GIOCATORI
-function trePlayers () {
-	var listaDue = ['d2','b2','c2','s2'];
-	var carta = [Math.floor(Math.random() * 4) + 1];
-	delete gameCards[listaDue[carta]];
-}
-
-//NUOVO GIOCO
-function nuovoGioco (){
-	var gameCards = card_list;
-}
 
 //CONTEGGIO PUNTI PARTITE PRECEDENTI
 
